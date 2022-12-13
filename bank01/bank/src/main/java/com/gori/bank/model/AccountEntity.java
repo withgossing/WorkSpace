38cafb025ecc0d8b21c2ed1e16afc0e55a53b70a
Seungyoun Ko <withgossing@outlook.com>
2022-12-13 19:39:52 +0900

    accountEntity 추가

---
+++ bank01/bank/src/main/java/com/gori/bank/model/AccountEntity.java
@@ -0,0 +1,11 @@
+package com.gori.bank.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountEntity {
+    private String AccountId;
+    private String AccountName;
+    private String UserId;
+}
