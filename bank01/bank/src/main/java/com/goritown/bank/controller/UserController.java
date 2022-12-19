69b6037abd5fadb191e01c92a62969a6cab8393a
Seungyoun Ko <withgossing@outlook.com>
2022-12-19 23:26:53 +0900

    폴더구조 추가 및 이름 변경

---
+++ bank01/bank/src/main/java/com/goritown/bank/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.goritown.bank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/User")
+public class UserController {
+    
+}
--- bank01/bank/src/main/java/com/goritown/bank/model/TransactionEntity.java
+++ bank01/bank/src/main/java/com/goritown/bank/model/TransactionEntity.java
@@ -1,9 +1,10 @@
-package com.goritown.bank.entity;
+package com.goritown.bank.model;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class TransactionEntity {
     private AccountEntity account;
 
